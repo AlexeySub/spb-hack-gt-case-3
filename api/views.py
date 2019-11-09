66f82b1f3fc188c0681cfda1b78f1d946d8ac58e
Vyacheslav <slavakasta@gmail.com>
2019-11-09 19:31:16 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -18,7 +18,7 @@ class MemberLogin(views.View):
         data = parsers.JSONParser().parse(request)
         print(data)
         print(request)
-        return member.login(data)
+        return member.login(request)
 
 
 class Role(views.View):
