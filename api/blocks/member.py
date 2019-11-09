4f989c5fcc826d3b9bab7283d3b4bf691ab64732
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-09 19:26:09 +0300

    Update member.py

--- api/blocks/member.py
+++ api/blocks/member.py
@@ -32,8 +32,8 @@ def get_members():
 
 def login(data):
     try:
-        member = Member.objects.get(email=data.POST['email'])
-        if member.password == func.Hash(data.POST['password']):
+        member = Member.objects.get(email=data['email'])
+        if member.password == func.Hash(data['password']):
             data.session['email'] = member.email
             return HttpResponse(renderers.JSONRenderer().render(member.values()))
     except Member.DoesNotExist:
