7ac5f2ac2a26990fe0d1962ef177953c1bb1620d
xegins <slavakasta@gmail.com>
2019-11-10 01:21:48 +0300

    channels

---
+++ api/consumers.py
@@ -0,0 +1,3 @@
+def ws_message(message):
+    data = message.content['text']
+    print(data)
--- spbhackgtcase3/settings.py
+++ spbhackgtcase3/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgiref.inmemory.ChannelLayer",
+        "ROUTING": "spbhackgtcase3.routing.channel_routing",
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
