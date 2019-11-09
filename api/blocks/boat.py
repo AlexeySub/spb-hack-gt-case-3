b0333d8f8be37c1da6c2ca5ffa02d2ec0733bfab
Vyacheslav <slavakasta@gmail.com>
2019-11-09 17:38:17 +0300

    Update boat.py

--- api/blocks/boat.py
+++ api/blocks/boat.py
@@ -5,7 +5,7 @@ from api.models import Boat, Team, BoatClass
 
 
 def register_boat(data):
-    boat = Boat(name=data['name'], boat_class=data['boat_class'], boat_number=data['boat_number'],
+    boat = Boat(name=data['name'], boat_class=int(data['boat_class']), boat_number=data['boat_number'],
                 tech_inspection=data['tech_inspection'])
 
     team = Team(boat_id=boat.id, member_id=data['member'])
@@ -28,4 +28,4 @@ def get_boat(data):
 
 def get_boat_class():
     classes = BoatClass.objects.all()
-    return HttpResponse(renderers.JSONRenderer().render(classes.values()[1]))
+    return HttpResponse(renderers.JSONRenderer().render(classes.values()))
