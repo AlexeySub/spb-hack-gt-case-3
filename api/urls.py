3e5513ceb0dd16728c81061cad8fbc893ee5de6c
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-09 18:02:21 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api import views
 
 urlpatterns = [
     path('member/', views.Member.as_view()),
-    path('member/login/', views.Member.as_view()),
+    path('member/login/', views.MemberLogin.as_view()),
     path('boat/', views.Boat.as_view()),
     path('role/', views.Role.as_view()),
     path('class/', views.Class.as_view()),
