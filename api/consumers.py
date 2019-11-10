82c4a9fd8170bc0f9c68b28946120f2fd2479d6d
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-10 03:00:38 +0300

    Update consumers.py

--- api/consumers.py
+++ api/consumers.py
@@ -2,3 +2,9 @@ def ws_message(message):
     # data = message.content['text']
     print(message.content)
     message.reply_channels.send("test_text")
+
+    
+def ws_add(message):
+    # Accept the connection
+    message.reply_channel.send({"accept": True})
+    
