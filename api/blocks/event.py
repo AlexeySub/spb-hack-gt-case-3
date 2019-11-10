1ce2268d1ad39f561331efdd743ece656133f383
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-10 12:41:58 +0300

    Create event.py

---
+++ api/blocks/event.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from rest_framework import renderers
+from api.models import Events
+
+
+def get_events():
+    events = Events.objects.all().values()
+    return HttpResponse(renderers.JSONRenderer().render(events.values))
+
+
+def get_event(data):
+    try:
+        event = Events.objects.get(id=data['id'])
+        return HttpResponse(renderers.JSONRenderer().render({'name':event.name, 'desc':event.description, 
+                                                             'date':event.date}))
+    except:
+        return HttpResponse(renderers.JSONRenderer().render({'error':'KEP'}))
