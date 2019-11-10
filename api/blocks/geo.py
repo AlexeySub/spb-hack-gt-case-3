5c8e81d2822cb7565287e652b435f1f11932e15b
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-10 10:16:24 +0300

    Update geo.py

--- api/blocks/geo.py
+++ api/blocks/geo.py
@@ -7,7 +7,9 @@ from api.models import Track
 def post_geo(data):
     try:
         track = Track.objects.filter(member_id=data['member']).update(coordinates=data['coordinates'])
+        track.save()
+        return HttpResponse(renderers.JSONRenderer().render({'coordinates': track.coordinates}))
     except:
         track = Track(member_id=data['member'], coordinates=data['coordinates'])
-    track.save()
-    return HttpResponse(renderers.JSONRenderer().render({'coordinates': track.coordinates}))
+        track.save()
+        return HttpResponse(renderers.JSONRenderer().render({'coordinates': track.coordinates}))
