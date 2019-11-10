bc0a83f12d5791a714bbeb663ad0185aecb03c00
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-10 03:02:59 +0300

    Update routing.py

--- spbhackgtcase3/routing.py
+++ spbhackgtcase3/routing.py
@@ -1,6 +1,7 @@
 from channels.routing import route
-from api.consumers import ws_message
+from api.consumers import ws_message, ws_add
 
 channel_routing = [
+    route("websocket.connect", ws_add, path=r'^/mobile/$'),
     route("websocket.receive", ws_message, path=r'^/mobile/$', method=r"^GET$"),
 ]
