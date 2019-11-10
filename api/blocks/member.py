fff1a2c6e10220c1e36c73acfdffa0c2159f6633
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-10 04:28:29 +0300

    Update member.py

--- api/blocks/member.py
+++ api/blocks/member.py
@@ -48,8 +48,8 @@ def get_members(data):
 def get_member(data):
     member = Member.objects.filter(id=data['id']).values('first_name', 'last_name', 'patronymic', 'email', 'phone_number',
                                                       'passport', 'swimming_skill')
-    member.update({'role':Role.objects.get(id=i.role_id).name, 'boat':Boat.objects.get(id=Team.objects.get(user_id=i.id).boat_id).name})
-    return HttpResponse(renderers.JSONRenderer().render(i))
+    member.update({'role':Role.objects.get(id=member.role_id).name, 'boat':Boat.objects.get(id=Team.objects.get(user_id=member.id).boat_id).name})
+    return HttpResponse(renderers.JSONRenderer().render(member.values()))
 
     
 def get_boat_members(data):
