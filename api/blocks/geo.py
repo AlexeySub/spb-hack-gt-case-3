8dfb1ac19eee1830e62e9b5cd256dd434afa0ba5
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-10 11:11:03 +0300

    Update geo.py

--- api/blocks/geo.py
+++ api/blocks/geo.py
@@ -5,13 +5,14 @@ from api.models import Track
 
 
 def post_geo(data):
-#     try:
-    Track.objects.filter(member_id=data['member']).update(coordinates=data['coordinates'])
-    return HttpResponse(renderers.JSONRenderer().render({'coordinates': data['coordinates']}))
-#     except:
-#         track = Track(member_id=data['member'], coordinates=data['coordinates'])
-#         track.save()
-#         return HttpResponse(renderers.JSONRenderer().render({'coordinates': track.coordinates}))
+    try:
+        track = Track.objects.get(member_id=data['member'])
+        Track.objects.filter(member_id=data['member']).update(coordinates=data['coordinates'])
+        return HttpResponse(renderers.JSONRenderer().render({'coordinates': data['coordinates']}))
+    except Track.DoesNotExist:
+        track = Track(member_id=data['member'], coordinates=data['coordinates'])
+        track.save()
+        return HttpResponse(renderers.JSONRenderer().render({'coordinates': track.coordinates}))
     
     
 def get_geo(data):
