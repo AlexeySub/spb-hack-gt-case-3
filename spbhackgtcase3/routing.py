881e5a77485ece5b1417cad95b2f4b64902d4979
xegins <slavakasta@gmail.com>
2019-11-10 05:23:07 +0300

    channels

--- spbhackgtcase3/routing.py
+++ spbhackgtcase3/routing.py
@@ -1,7 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import api.routing
+from django.conf.urls import url
+from api import consumers
 
 application = ProtocolTypeRouter({
-    'websocket': URLRouter(api.routing.websocket_urlpatterns)
+    'websocket': URLRouter([
+        url("ws://spb-hack-gt-case-3.herokuapp.com/mobile/", consumers.Consumer)
+    ])
 })
